9c9eabca1fbba9c337f27808e51c0246c206f28a
Matias <maticapuano97@gmail.com>
2020-07-26 17:49:56 -0500

    Implemented event services.

---
+++ event/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const axios = require("axios").default;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/events", async (req, res) => {
+  const event = req.body;
+
+  await axios.post("http://localhost:4000/events", event);
+  await axios.post("http://localhost:4001/events", event);
+  //  await axios.post("http://localhost:4002/events", event);
+
+  return res.json({ status: "OK" });
+});
+
+app.listen(5002, () => console.log("Server ready on port 5002"));
