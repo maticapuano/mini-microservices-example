98026e98578b00b28bb60a66a09d3fe47587099b
Matias <maticapuano97@gmail.com>
2020-07-27 16:57:06 -0500

    Added services moderation.

--- moderation/src/server.js
+++ moderation/src/server.js
@@ -7,7 +7,26 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "comment.created") {
+    const status = data.content.includes(["orange", "bad", "fuck"])
+      ? "rejected"
+      : "approved";
+
+    await axios.post("http://localhost:5002/events", {
+      type: "comments.moderated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.json({});
+});
 
 app.listen(4003, () => {
   console.log("Server ready on port 4003");
