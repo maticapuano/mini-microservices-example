827cc830353828ca2a916b56e8aa8788669f52a9
Matias <maticapuano97@gmail.com>
2020-07-27 20:18:27 -0500

    Added comment moderation.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,22 @@ import React from "react";
 
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}> {comment.content}</li>;
+    let content;
+
+    //console.log(comment);
+    if ((comment.type = "approved")) {
+      content = comment.content;
+    }
+
+    if (comment.type === "pending") {
+      content = "This comment is awaiting for moderation.";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}> {content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
